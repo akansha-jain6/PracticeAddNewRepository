03984a4ae2c8bd92d31c3ea372c991dbdee6a12d
Akansha Jain <goyanka.akku@gmail.com>
2021-08-31 09:12:39 +0530

    Changes

--- TestAkansha/src/AkanshaJain/Example1.java
+++ TestAkansha/src/AkanshaJain/Example1.java
@@ -4,7 +4,5 @@ public class Example1 {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Akansha");
-		System.out.println("New Line for Branch A");
-		System.out.println("New Line");
 	}
 }
